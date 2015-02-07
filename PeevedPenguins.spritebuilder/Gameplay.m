e507ee60eed780905d3bb272b57109c7fd40fc5c
Yanna Wu <wuyanna2009@gmail.com>
2015-02-06 23:26:57 -0800

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -34,6 +34,8 @@
     // nothing shall collide with our invisible nodes
     _pullbackNode.physicsBody.collisionMask = @[];
     _mouseJointNode.physicsBody.collisionMask = @[];
+
+    _physicsNode.collisionDelegate = self;
 }
 
 // called on every touch in this scene
@@ -127,6 +129,11 @@
 - (void)retry {
     // reload this level
     [[CCDirector sharedDirector] replaceScene: [CCBReader loadAsScene:@"Gameplay"]];
+}
+
+-(void)ccPhysicsCollisionPostSolve:(CCPhysicsCollisionPair *)pair seal:(CCNode *)nodeA wildcard:(CCNode *)nodeB
+{
+    CCLOG(@"Something collided with a seal!");
 }
 
 @end
--- PeevedPenguins.spritebuilder/Seal.m
+++ PeevedPenguins.spritebuilder/Seal.m
@@ -10,5 +10,8 @@
 
 @implementation Seal
 
+- (void)didLoadFromCCB {
+    self.physicsBody.collisionType = @"seal";
+}
 
 @end

8f98aef92d96756dc7390724c11152dac32dd3a1
Yanna Wu <wuyanna2009@gmail.com>
2015-02-07 00:10:33 -0800

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/WaitingPenguin.h
@@ -0,0 +1,13 @@
+//
+//  WaitingPenguin.h
+//  PeevedPenguins
+//
+//  Created by Yanna on 2/7/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface WaitingPenguin : CCSprite
+
+@end
